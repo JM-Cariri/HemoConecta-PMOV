9f3f7df8741c7cf84ed2338f7277b92017d7ca5c
JM-Cariri <jmcs10@aluno.ifal.edu.br>
2026-05-25 10:07:22 -0300

    configurações iniciais

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// app/index.tsx
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello, HemoConecta!</Text>
+    </View>
+  );
+}
